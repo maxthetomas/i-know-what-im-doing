42c201b1bb9eeeb1a33cde147a29e6aa7060d6e8
maxthetomas <max@maxthetomas.ru>
2023-07-07 18:10:40 +0300

    Initial commit

---
+++ src/main/java/ru/maxthetomas/ikwid/iknowwhatimdoing/IKnowWhatImDoing.java
@@ -0,0 +1,8 @@
+package ru.maxthetomas.ikwid.iknowwhatimdoing;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class IKnowWhatImDoing implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() { }
+}
