616e1cfcc4b3726b0ea7dbe08a2ae3f55c6a4991
maxthetomas <max@maxthetomas.ru>
2023-12-18 13:39:23 +0300

    Updated to support 1.20.3-1.20.4

--- src/main/java/ru/maxthetomas/ikwid/iknowwhatimdoing/mixin/IntegratedServerLoaderMixin.java
+++ src/main/java/ru/maxthetomas/ikwid/iknowwhatimdoing/mixin/IntegratedServerLoaderMixin.java
@@ -7,7 +7,7 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(IntegratedServerLoader.class)
 public abstract class IntegratedServerLoaderMixin {
-    @ModifyVariable(method = "start(Lnet/minecraft/client/gui/screen/Screen;Ljava/lang/String;ZZ)V", at = @At("HEAD"), index = 3, argsOnly = true, ordinal = 1, name = "canShowBackupPrompt")
+    @ModifyVariable(method = "start(Lnet/minecraft/world/level/storage/LevelStorage$Session;Lcom/mojang/serialization/Dynamic;ZZLjava/lang/Runnable;)V", at = @At("HEAD"), index = 3, argsOnly = true, ordinal = 1, name = "canShowBackupPrompt")
     boolean modifyCanShowBackupPrompt(boolean value) {
         return false;
     }
