d3dab1fa3eb66edcf4a51b9b2db1bde49e6d597a
maxthetomas <max@maxthetomas.ru>
2024-07-22 22:56:28 +0300

    Updated to support 1.20.5-1.21

--- src/main/java/ru/maxthetomas/ikwid/iknowwhatimdoing/mixin/IntegratedServerLoaderMixin.java
+++ src/main/java/ru/maxthetomas/ikwid/iknowwhatimdoing/mixin/IntegratedServerLoaderMixin.java
@@ -1,14 +1,19 @@
 package ru.maxthetomas.ikwid.iknowwhatimdoing.mixin;
 
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.server.integrated.IntegratedServerLoader;
+import net.minecraft.world.level.storage.LevelStorage;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(IntegratedServerLoader.class)
 public abstract class IntegratedServerLoaderMixin {
-    @ModifyVariable(method = "start(Lnet/minecraft/world/level/storage/LevelStorage$Session;Lcom/mojang/serialization/Dynamic;ZZLjava/lang/Runnable;)V", at = @At("HEAD"), index = 3, argsOnly = true, ordinal = 1, name = "canShowBackupPrompt")
-    boolean modifyCanShowBackupPrompt(boolean value) {
-        return false;
+    @Inject(at = @At("HEAD"), method = "showBackupPromptScreen", cancellable = true)
+    public void showBackupPrompt(LevelStorage.Session session, boolean customized, Runnable callback, Runnable onCancel, CallbackInfo ci) {
+        if (customized) return;
+        ci.cancel();
+        MinecraftClient.getInstance().execute(callback);
     }
 }
